0ca6f5e8cacfb9ccccde6f3e1bd17f3725563503
Hameed Hussain <94852333@aquarium.com>
2019-10-09 10:33:27 +0400

    Github commit

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,20 @@ Route::get('/', function () {
 
 Auth::routes();
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This is test github
+|
+*/
+
 Route::get('/admin', 'HomeController@index')->name('home');
 Route::get('/about', 'TestController@about')->name('about');
 Route::get('/product', 'TestController@product')->name('product');
 Route::get('/testhome', 'HomeController@test')->name('testhome');
-Route::get('/dashboard', 'TestController@dashboard')->name('dashboard');
+Route::get('/dashboard', 'TestController@dashboard')->name('dashboard'); -- Test
 
 
 Route::get('/admin/accounts', 'AccountsController@reimbursement')->name('reimbursement');
